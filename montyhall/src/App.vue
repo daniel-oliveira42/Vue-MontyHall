85b9180599823af1214d30d8d2d2002601b46c8e
Daniel André Lima <65387662+daniel-oliveira42@users.noreply.github.com>
2021-06-29 19:57:33 -0300

    Vue client create

---
+++ montyhall/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>Monty Hall Problem</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ * {
+   box-sizing: border-box;
+   font-family: 'Montserrat', sans-serif;
+ }
+
+ body {
+    background: #659999;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #f4791f, #659999);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #f4791f, #659999); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+ }
+
+ #app {
+   display: flex;
+   flex-direction: column
+ }
+
+ #app h1 {
+   color: white;
+   text-align: center;
+ }
+
+
+</style>
